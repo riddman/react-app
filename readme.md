e9f44389a750fe6b925bd39bcfec1a74648d7a90
riddman <riddleman@ukr.net>
2023-10-31 23:57:59 +0200

    Init

---
+++ readme.md
@@ -0,0 +1,4 @@
+node veriosn 20.9.0
+npm run dev
+
+https://getbootstrap.com/docs/5.3/components/list-group/
---
+++ src/Massage.tsx
@@ -0,0 +1,7 @@
+function Message() {
+    // JSX: JavaScript XML
+    const name = 'Riddman';
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
