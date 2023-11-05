64b9f0a5c0b6b1f87fd10476732e1af378c82d0a
riddman <riddleman@ukr.net>
2023-11-06 00:15:19 +0200

    Dynamic List. Fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,33 @@
+import { Fragment } from 'react';
+
 function ListGroup() {
-  return (
-    <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-    </ul>
-  );
+    let items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    // items = [];
+
+    const emptyMessage = () => {
+        return items.length === 0 ? <p>No item found</p> : null;
+    }
+
+    return (
+        <Fragment>
+            <h1>List</h1>
+            {emptyMessage()}
+            <ul className="list-group">
+                {items.map((item, index) => {
+                    return <li key={index} className="list-group-item">{item}</li>
+                })}
+                <li className="list-group-item">An item</li>
+         </ul>
+        </Fragment>
+    
+    );
 }
 
 export default ListGroup;
