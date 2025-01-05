573942cb42f82f03c89b9886e796b01261509fa8
riddman <riddleman@ukr.net>
2025-01-05 21:56:18 +0200

    Usage of react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,33 +1,39 @@
-import React, { FormEvent, useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import { FieldValues, useForm } from 'react-hook-form';
+
+interface FormData {
+    name: string,
+    age: number
+}
 
 function Form() {
+    const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
+
     const nameRef = useRef<HTMLInputElement>(null);
     const ageRef = useRef<HTMLInputElement>(null);
-    const person = {name: '', age: 0};
+    const [person, setPerson] = useState({name: '', age: 0});
 
-    const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        if (nameRef.current !== null) {
-            person.name = nameRef.current.value;
-        }
+    console.log(register('name', { required: true, minLength: 3} ));
 
-        if (ageRef.current !== null) {
-            person.age = parseInt(ageRef.current.value);
-        }
-
-        console.log(person);
-
-    };
+    const onSubmit = (data: FieldValues) => console.log(data);
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(data => console.log(data))}>
             <div className="mb-3">
                 <label htmlFor="name" className="from-label">Name</label>
-                <input ref={nameRef} id="name" type="text" className="form-control" />
+                <input
+                    { ...register('name') }
+                    // onChange={(event) => setPerson({...person, name: event.target.value})}
+                    id="name" type="text" className="form-control" />
+                { errors.name?.type === 'required' && <p className="text-danger">The name field is required</p> }
+                { errors.name?.type === 'minLength' && <p className="text-danger">The name should be at least 3 characters</p> }
             </div>
             <div className="mb-3">
                 <label htmlFor="age" className="form-label"></label>
-                <input ref={ageRef} id="age "type="number" className='form-control' />
+                <input
+                    { ...register('age') }
+                    // onChange={(event) => setPerson({...person, age: parseInt(event.target.value)})}
+                    id="age "type="number" className='form-control' />
             </div>
             <button className="btn btn-primary" type="submit">Submit</button>
         </form>
